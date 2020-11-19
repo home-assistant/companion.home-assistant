272feea685a04c433730efb7091d48ab173537ba
Tom Brien <tom@byth.co.uk>
2020-11-19 14:40:01 +0000

    Auto linting

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,9 +15,9 @@ module.exports = {
         srcDark: 'img/logo-pretty.svg',
       },
       items: [
-        {to: '/docs/getting_started/getting-started', label: 'Docs', position: 'left'},
-        {to: '/download', label: 'Download', position: 'left'},
-        {to: '/docs/troubleshooting/more-help', label: 'Support', position: 'left'},
+        { to: '/docs/getting_started/getting-started', label: 'Docs', position: 'left' },
+        { to: '/download', label: 'Download', position: 'left' },
+        { to: '/docs/troubleshooting/more-help', label: 'Support', position: 'left' },
         {
           href: 'https://www.github.com/home-assistant/iOS',
           label: 'GitHub (iOS)',
