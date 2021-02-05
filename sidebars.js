aa38646bbe8ef6b38666ffa2b82a0720d596c50f
Daniel Shokouhi <dshokouhi@gmail.com>
2021-02-05 08:46:27 -0800

    Docs for android weblink overrides (#450)

--- sidebars.js
+++ sidebars.js
@@ -35,6 +35,7 @@ module.exports = {
     'Integrations': [
       'integrations/integrations',
       'integrations/android-power-menu',
+      'integrations/android-webview-links',
       'integrations/android-widgets',
       'integrations/app-events',
       'integrations/haptics',
