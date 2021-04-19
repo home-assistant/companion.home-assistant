d75806df554d965aa909aaa1ef877614792281c6
Tom Brien <tom@byth.co.uk>
2021-04-19 15:00:56 +0100

    Replace classnames with clsx
    As per Docusaurus's current site

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classnames from 'classnames';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -12,11 +12,11 @@ function Hello() {
     <Layout
       title="Download"
       description="Welcome to the Home Assistant App docs">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">Download the Home Assistant Apps</h1>
           <p className="hero__subtitle">Get the apps now!</p>
-          <div className={classnames(styles.buttons, 'download-badges')} >
+          <div className={clsx(styles.buttons, 'download-badges')} >
           <a href="https://apps.apple.com/us/app/home-assistant/id1099568401?mt=8" style={{display:'inline-block', overflow: 'hidden', background: 'url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2017-04-15&kind=iossoftware&bubble=apple_music) no-repeat center', width: '155px', height: '40px'}}></a>
           <a href='https://play.google.com/store/apps/details?id=io.homeassistant.companion.android&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
             <img alt='Get it on Google Play' width="155" src='https://play.google.com/intl/en_gb/badges/static/images/badges/en_badge_web_generic.png'/>
