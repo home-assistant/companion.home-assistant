6ff8d5c3bfc49d04f67a259a4537f6f708609716
tzagim <2285958+tzagim@users.noreply.github.com>
2022-04-22 13:10:30 +0300

    Update download.js

--- src/pages/download.js
+++ src/pages/download.js
@@ -20,6 +20,9 @@ function Hello() {
             <a href='https://play.google.com/store/apps/details?id=io.homeassistant.companion.android&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
               <img width="200" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' />
             </a>
+            <a href='https://f-droid.org/packages/io.homeassistant.companion.android.minimal'>
+              <img width="200" alt='Get it on F-Droid' src='https://fdroid.gitlab.io/artwork/badge/get-it-on.png' />
+            </a>
             <a href="https://sidequestvr.com/app/6427/home-assistant">
               <img width="175" src="https://sidequestvr.com/assets/images/branding/Get-it-on-SIDEQUEST.png" alt="Download on SideQuest" />
             </a>
