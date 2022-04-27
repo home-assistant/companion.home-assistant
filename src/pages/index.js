c9b100e94b390983cf3fc4cee2ca9225bddca187
tzagim <2285958+tzagim@users.noreply.github.com>
2022-04-27 19:15:53 +0300

    Add F-Droid (#735)
    * Add F-Droid
    
    * FIX width
    
    * Multi Lang
    
    * Update download.js
    
    * FIX spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,11 +45,15 @@ const features = [
     description: (
       <>
         <a href='https://play.google.com/store/apps/details?id=io.homeassistant.companion.android&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' style={{ display: 'inline-block' }}>
-          <img width="200" class="download-badge" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' />
+          <img class="download-badge" width="200" src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" alt='Get it on Google Play' />
         </a>
         <br />
         <a href="https://apps.apple.com/us/app/home-assistant/id1099568401?itsct=apps_box_badge&amp;itscg=30200" style={{ display: 'inline-block', width: '200px' }}>
           <img class="download-badge" width="175" src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1492214400&h=3ef4307fa479838e52fe9bd8bd17913b" alt="Download on the App Store" />
+        </a>
+        <br />
+        <a href="https://f-droid.org/packages/io.homeassistant.companion.android.minimal" style={{ display: 'inline-block', width: '200px' }}>
+          <img class="download-badge" width="200" src="https://fdroid.gitlab.io/artwork/badge/get-it-on.png" alt="Get it on F-Droid" />
         </a>
         <br />
         <a href="https://sidequestvr.com/app/6427/home-assistant" style={{ display: 'inline-block', width: '200px' }}>
