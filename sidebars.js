d9aff97f0aa2f420d116a6577898b5a0eabb2d1d
Timothy <6560631+TimoPtr@users.noreply.github.com>
2025-12-22 08:46:07 +0100

    Companion app as home app (#1268)
    ---------
    
    Co-authored-by: c0ffeeca7 <38767475+c0ffeeca7@users.noreply.github.com>
    Co-authored-by: Joris Pelgröm <jpelgrom@users.noreply.github.com>

--- sidebars.js
+++ sidebars.js
@@ -39,6 +39,7 @@ module.exports = {
     'Integrations': [
       'integrations/integrations',
       'integrations/android-device-controls',
+      'integrations/android-home-app-launcher',
       'integrations/android-quick-settings',
       'integrations/android-shortcuts',
       'integrations/android-webview',
